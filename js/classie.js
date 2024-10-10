132f9acb56f7ee092a11b58e45e19f6c7b9c843b
Carlos Falcón <carlosbch021@gmail.com>
2024-10-10 13:08:24 +0100

    Fin de la página

--- js/classie.js
+++ js/classie.js
@@ -11,6 +11,55 @@
 /*jshint browser: true, strict: true, undef: true */
 /*global define: false */
 
+var deluxeSwiper = new Swiper('.swiper-deluxe', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-deluxe .swiper-button-next', // Específico para deluxe
+    prevEl: '.swiper-deluxe .swiper-button-prev', // Específico para deluxe
+  },
+});
+
+var deluxeViewSwiper = new Swiper('.swiper-deluxe-view', {
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-deluxe-view .swiper-button-next', // Específico para deluxe view
+    prevEl: '.swiper-deluxe-view .swiper-button-prev', // Específico para deluxe view
+  },
+});
+
+// Mostrar el pop-up al cargar la página
+document.addEventListener('DOMContentLoaded', function() {
+  var cookiePopup = document.getElementById('cookie-popup');
+  cookiePopup.style.display = 'block'; // Mostrar el pop-up
+  
+  // Manejar el clic en el botón "Aceptar"
+  document.getElementById('accept-cookies').addEventListener('click', function() {
+      cookiePopup.style.display = 'none'; // Ocultar el pop-up
+      // Aquí puedes agregar lógica para guardar la aceptación de cookies, si es necesario
+  });
+  
+  // Manejar el clic en el botón "Denegar"
+  document.getElementById('deny-cookies').addEventListener('click', function() {
+      alert('No puedes acceder a la página.'); // Mensaje informativo
+      window.location.href = "https://www.google.com"; // Redirigir a otra página (ejemplo)
+  });
+  
+  // Manejar el clic en el botón de cerrar
+  document.getElementById('close-popup').addEventListener('click', function() {
+      cookiePopup.style.display = 'none'; // Ocultar el pop-up
+  });
+});
+
+
+
 ( function( window ) {
 
 'use strict';
